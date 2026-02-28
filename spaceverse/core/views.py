cdd9480b3490833ddbf44e814022e7850a9dc265
ShuklaDhruv1 <dhruvshukla617@gmail.com>
2026-03-01 00:14:12 +0530

    Add authentication: signup, login, logout with templates

--- spaceverse/core/views.py
+++ spaceverse/core/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 def home(request):
     return render(request, 'core/home.html')
+
+def signup(request):
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, "Account created! Please login.")
+            return redirect('login')
+    else:
+        form = UserCreationForm()
+    return render(request, 'core/signup.html', {'form': form})
