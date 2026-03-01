4cd41f271a507fe7ca1c3d184e5b03c737f27500
ShuklaDhruv1 <dhruvshukla617@gmail.com>
2026-03-02 00:48:55 +0530

    Add search feature

--- spaceverse/core/urls.py
+++ spaceverse/core/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import home, signup
+from .views import home, signup, search
 
 urlpatterns = [
     path('', home, name='home'),
     path('signup/', signup, name='signup'),
+    path('search/', search, name='search'),
 ]
--- spaceverse/core/views.py
+++ spaceverse/core/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from blog.models import Post
+from spacepedia.models import SpaceTopic
+from django.db.models import Q
 
 def home(request):
     return render(request, 'core/home.html')
@@ -15,3 +18,11 @@ def signup(request):
     else:
         form = UserCreationForm()
     return render(request, 'core/signup.html', {'form': form})
+
+def search(request):
+    q = request.GET.get('q', '').strip()
+    posts = topics = []
+    if q:
+        posts = Post.objects.filter(Q(title__icontains=q) | Q(content__icontains=q))
+        topics = SpaceTopic.objects.filter(Q(title__icontains=q) | Q(content__icontains=q))
+    return render(request, 'core/search.html', {'q': q, 'posts': posts, 'topics': topics})
